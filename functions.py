fcb7f5fc520ad5d4cfcef9c6f1b91333faa4f63d
kruti-w <sanskruti290@hmail.com>
2024-05-14 17:08:42 -0700

    Added functions

---
+++ functions.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+
+def add_user():
+    return
+
+
+def edit_users():
+    return
+
+
+def add_plates(plates_list):
+    return
+
+
+def edit_plates(plates_list):
+    return
+
+
+def output_plates():
+    return
+
+
+def delete_user():
+    return
+
+
+def delete_plate():
+    return
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 
 def main():
     root = tk.Tk()
     root.title("Barbell Weights Configuration")
+    root.geometry("800x800")
 
+    frm = ttk.Frame(root, padding=250)
+    frm.grid()
+    ttk.Label(frm, text="Testing").grid(column=3, row=5)
+    ttk.Label(frm, text="Testing").grid(column=10, row=5)
     root.mainloop()
 
 
