32e64693fb08d8cd44c8f582e62936b01d895b73
kruti-w <sanskruti290@hmail.com>
2024-05-15 00:11:12 -0700

    Added functions to add users and plates, new windows for on-click events

--- functions.py
+++ functions.py
@@ -1,8 +1,19 @@
 from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
 
-def add_user():
-    return
+def add_user(users_list):
+    users_window = tk.Toplevel()
+    users_window.title("Modify Users")
+    users_window.geometry("450x450")
+
+    label = tk.Label(users_window, text="View Users")
+    for user in users_list:
+        user_label = tk.Label(users_window, text=user)
+        user_label.grid()
+
+    label.grid()
 
 
 def edit_users():
@@ -10,7 +21,16 @@ def edit_users():
 
 
 def add_plates(plates_list):
-    return
+    plates_window = tk.Toplevel()
+    plates_window.title("Modify Plates")
+    plates_window.geometry("450x450")
+
+    label = tk.Label(plates_window, text="This is the list of all the plates you have")
+    for plate in plates_list:
+        plate_label = tk.Label(plates_window, text=plate)
+        plate_label.grid()
+
+    label.grid()
 
 
 def edit_plates(plates_list):
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from functions import *
+
+p_list = ["0"]
+u_list = ["User1", "User2"]
 
 
 def main():
@@ -13,9 +17,9 @@ def main():
     style.configure("TFrame", background="green")
     frm.grid()
     ttk.Label(frm, text="Users: ").grid(column=3, row=5)
-    editUsersBtn = ttk.Button(frm, text="EDIT")
+    editUsersBtn = ttk.Button(frm, text="ADD OR EDIT", command=lambda: add_user(u_list))
     editUsersBtn.grid(column=690, row=5)
-    editPlatesBtn = ttk.Button(frm, text="EDIT")
+    editPlatesBtn = ttk.Button(frm, text="ADD OR EDIT", command=lambda: add_plates(p_list))
     editPlatesBtn.grid(column=690, row=15)
 
     ttk.Label(frm, text="Plates: ").grid(column=3, row=15)
@@ -23,8 +27,8 @@ def main():
     frm2 = ttk.Frame(root, height=300, width=700)
     frm2.grid()
 
-
-
+    input_str1 = tk.Entry(frm2)
+    input_str1.grid(column=10, row=25)
 
     root.mainloop()
 
