8b3de866dd01360109f7b617a9bca7d928c301bd
kruti-w <sanskruti290@hmail.com>
2024-05-14 17:53:04 -0700

    Added frames, buttons and some styling

--- main.py
+++ main.py
@@ -8,10 +8,24 @@ def main():
     root.title("Barbell Weights Configuration")
     root.geometry("800x800")
 
-    frm = ttk.Frame(root, padding=250)
+    frm = ttk.Frame(root, height=100, width=700, borderwidth=10)
+    style = ttk.Style()
+    style.configure("TFrame", background="green")
     frm.grid()
-    ttk.Label(frm, text="Testing").grid(column=3, row=5)
-    ttk.Label(frm, text="Testing").grid(column=10, row=5)
+    ttk.Label(frm, text="Users: ").grid(column=3, row=5)
+    editUsersBtn = ttk.Button(frm, text="EDIT")
+    editUsersBtn.grid(column=690, row=5)
+    editPlatesBtn = ttk.Button(frm, text="EDIT")
+    editPlatesBtn.grid(column=690, row=15)
+
+    ttk.Label(frm, text="Plates: ").grid(column=3, row=15)
+
+    frm2 = ttk.Frame(root, height=300, width=700)
+    frm2.grid()
+
+
+
+
     root.mainloop()
 
 
